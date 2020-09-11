82163564e3a58a324cbfbba20cc99a335993d420
Vincent Bongartz <110112@st-maartenscollege.nl>
2020-09-11 09:13:21 +0200

    ik heb verder gewerkt aan het contact gedeelte

---
+++ SchoolTemplate/Models/PersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SchoolTemplate.Models
+{
+    public class PersonModel
+    {
+        public string Voornaam { get; set; }
+        
+        [Required(ErrorMessage = "Achternaam is verplicht")]
+        public string Achternaam { get; set; }
+        
+        [Required(ErrorMessage ="E-mail is verplicht")]
+        [EmailAddress(ErrorMessage = "Geen geldig e-mail adres")]
+        public string Email { get; set; }
+
+        public DateTime Geboortedatum { get; set; }
+        
+    }
+}
