48d852be65f9545d0456b623a0d7c9bcbc2ff32c
Vincent Bongartz <110112@st-maartenscollege.nl>
2020-10-02 07:01:44 +0000

    image hopelijk verplaatst en informatica.st-maartenscollege.nl

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchoolTemplate.Controllers
     public class HomeController : Controller
     {
         // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-        string connectionString = "Server=172.16.160.21;Port=3306;Database=109750;Uid=109750;Pwd=SIDEpROp;";
+        string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=109750;Uid=109750;Pwd=SIDEpROp;";
 
         public IActionResult Index()
         {
